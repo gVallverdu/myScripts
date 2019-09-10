1bb4aa8b8b8633d683c385e498355a75a4457316
Germain Salvato-Vallverdu <germain.vallverdu@gmail.com>
2019-09-10 09:39:01 +0200

    update makeCL.py

--- VASP/makeCL.py
+++ VASP/makeCL.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-from __future__ import division, print_function
 import os
 import shutil
+import pymatgen as mg
+from pymatgen.io.vaspio.vasp_input import Poscar, Potcar
+
 
 def qnumber(oa):
-    """ 
+    """
     return n and l quantum number from an atomic orbital defined as a string.
 
     args:
@@ -32,11 +34,12 @@ def qnumber(oa):
 
     return n, l
 
+
 def add_CL_tags(oa, clz=.5):
-    """ 
+    """
     print core level part of INCAR file. The atom for which calculation is done, is
     supposed to be the first one (CLNT = 1).
-    
+
     args:
         oa (str): atomic orbital such as '1s', '3p' ...
         clz (float): CLZ value
@@ -44,7 +47,7 @@ def add_CL_tags(oa, clz=.5):
 
     n, l = qnumber(oa)
 
-    line = "Core Level\n"
+    line = "\nCore Level\n"
     line += "  ICORELEVEL = 2\n"
     line += "  CLNT = 1\n"
     line += "  LVTOT = .TRUE.\n"
@@ -54,12 +57,12 @@ def add_CL_tags(oa, clz=.5):
 
     return line
 
+
 def buildPOTCAR(atname, fpotcar="POTCAR"):
     """
     Build POTCAR file according to atom for wich CL calculation is required. The POTCAR of
     the atom is placed in first postion.
     """
-    from pymatgen.io.vaspio.vasp_input import Potcar
     potcar = Potcar.from_file(fpotcar)
     add = False
     for pot in potcar:
@@ -75,19 +78,31 @@ def buildPOTCAR(atname, fpotcar="POTCAR"):
 
     potcar.write_file("POTCAR_CL")
 
+
 def buildPOSCAR(iat, fposcar="POSCAR"):
     """
     Build POSCAR file according to atom for wich CL calculation is required. The
     corresponding atom is placed in first position in the POSCAR.
     """
-    from pymatgen.io.vaspio.vasp_input import Poscar
-    poscar = Poscar.from_file(fposcar)
+    poscar = Poscar.from_file(fposcar, read_velocities=False)
 
+    # move up the selected atom and ad "_" to differentiate it
     site = poscar.structure.pop(iat)
-    poscar.structure.insert(0, site.specie, site.frac_coords)
+    poscar.structure.insert(0, mg.DummySpecie("_" + site.specie.symbol), site.frac_coords)
+
     poscar.write_file("POSCAR_CL.vasp")
 
-def makeRun(ao, job, clz=.5, poscar="POSCAR_CL.vasp", potcar="POTCAR_CL", incar="INCAR_CL"):
+    # remove the _ symbol on 6th line
+    with open("POSCAR_CL.vasp", "r") as f:
+        lines = f.readlines()
+        lines[5] = "".join(["%4s" % el.strip("_") for el in lines[5].split()]) + "\n"
+        lines = "".join(lines)
+    with open("POSCAR_CL.vasp", "w") as f:
+        f.write(lines)
+
+
+def makeRun(ao, job, clz=.5, poscar="POSCAR_CL.vasp", potcar="POTCAR_CL",
+            incar="INCAR_CL", dirname=None):
     """
     Setup the job:
 
@@ -100,7 +115,7 @@ def makeRun(ao, job, clz=.5, poscar="POSCAR_CL.vasp", potcar="POTCAR_CL", incar=
     Note that POTCAR and POSCAR file have to be previously checked. The atom for which the
     corelevel calculation will be done is the first one. See buildPOTCAR and buildPOSCAR
     functions.
-    
+
     Args:
         oa (str): atomic orbital such as '1s', '3p' ...
         job (str): name of job file to submit calculation.
@@ -108,6 +123,7 @@ def makeRun(ao, job, clz=.5, poscar="POSCAR_CL.vasp", potcar="POTCAR_CL", incar=
         poscar (str): name of POSCAR file.
         potcar (str): name of POTCAR file.
         incar (str): name of the initial INCAR file.
+        dirname (str): name of the job directory, if None "CL_%3.1f" % clz is used
 
     Return:
         dirname (str): name of the job directory
@@ -115,7 +131,9 @@ def makeRun(ao, job, clz=.5, poscar="POSCAR_CL.vasp", potcar="POTCAR_CL", incar=
     """
 
     # make job directory
-    dirname = os.path.join(os.getcwd(), "CL_%3.1f" % clz)
+    if not dirname:
+        dirname = "CL_%3.1f" % clz
+    dirname = os.path.join(os.getcwd(), dirname)
     os.mkdir(dirname)
 
     # copy files
@@ -126,23 +144,39 @@ def makeRun(ao, job, clz=.5, poscar="POSCAR_CL.vasp", potcar="POTCAR_CL", incar=
     shutil.copy(job, os.path.join(dirname, jobname))
 
     # complete and copy INCAR file
-    incar = open(incar, "r").read() + add_CL_tags("1s", clz)
-    open(dirname + "/INCAR", "w").write(incar)
+    with open(incar, "r") as fincar:
+        incar_lines = fincar.read() + add_CL_tags("1s", clz)
+    with open(os.path.join(dirname, "INCAR"), "w") as fincar:
+        fincar.write(incar_lines)
 
     return dirname, jobname
 
 if __name__ == "__main__":
 
-    # pic de coeur O1s
-    buildPOTCAR("O")
+    # pic de coeur Li1s
+    buildPOTCAR("Li", "../opt/POTCAR")
 
-    # O71 est le plus proche du Si central
-    buildPOSCAR(71, "CONTCAR")
+    # make super cell
+    struct = mg.Structure.from_file("../opt/CONTCAR")
+    struct.make_supercell(4)
 
-    zval = [x/10 for x in range(11)]
+    p = mg.io.vasp.inputs.Poscar(struct)
+    p.write_file("POSCAR_4.vasp")
+
+    buildPOSCAR(1, "POSCAR_4.vasp")
+
+    # make runs
+    zval = [0., 0.2, 0.4, 0.5, 0.6, 0.8, 1.]
     print(zval)
 
+    lines = "#!/bin/bash\n"
     for clz in zval:
-        dir, job = makeRun("1s", "jSiO2", clz)
-        print("cd " + dir)
-        print("qsub " + job)
+        dirname, job = makeRun("1s", "jLi_bcc", clz)
+        dirname = "/" + dirname.strip("/Users/gvallver/pyrene")
+        print("cd " + dirname)
+        print("sbatch " + job)
+        lines += "cd %s \n" % dirname
+        lines += "sbatch %s \n" % job
+
+    with open("lancer.sh", "w") as f:
+        f.write(lines)
